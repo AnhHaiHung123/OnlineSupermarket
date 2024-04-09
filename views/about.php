9e98a872f00cab27a295e8afdb9957d88058db68
AnhHaiHung123 <phamdongviethung06122002@gmail.com>
2024-04-09 22:46:25 +0700

    add file html,css,js

---
+++ views/about.php
@@ -0,0 +1,27 @@
+<?= titlePage("Giới thiệu") ?>
+<main>
+    <article>
+        <div id="aboutus">
+            <h1>Giới thiệu</h1>
+            <div>
+                <p>
+                **Giới Thiệu về Online Express Supermarket:**
+
+Online Express Supermarket là một nền tảng mua sắm trực tuyến mang đến trải nghiệm mua sắm thuận tiện và đa dạng cho mọi người. Với sự kết hợp của công nghệ và dịch vụ chăm sóc khách hàng chuyên nghiệp, chúng tôi cam kết cung cấp những sản phẩm chất lượng cao và dịch vụ hoàn hảo để đáp ứng nhu cầu mua sắm hàng ngày của mọi gia đình.
+
+Chúng tôi cung cấp một loạt các sản phẩm từ các danh mục đa dạng như thực phẩm tươi sống, thực phẩm đóng hộp, đồ gia dụng, thiết bị điện tử, thời trang, đồ dùng cá nhân và nhiều hơn nữa. Với sự đa dạng này, khách hàng có thể tìm thấy mọi thứ mình cần cho cuộc sống hàng ngày trong một nơi duy nhất.
+
+Với giao diện người dùng thân thiện và dễ sử dụng, việc mua sắm trên Online Express Supermarket trở nên dễ dàng và tiện lợi hơn bao giờ hết. Khách hàng có thể duyệt qua danh sách sản phẩm, thêm vào giỏ hàng và hoàn tất quá trình thanh toán chỉ trong vài bước đơn giản.
+
+Chúng tôi cam kết đảm bảo việc giao hàng nhanh chóng và an toàn đến tận cửa nhà của khách hàng. Với hệ thống giao hàng hiện đại và đội ngũ nhân viên giao hàng chuyên nghiệp, chúng tôi luôn cố gắng để đảm bảo rằng mỗi đơn hàng đều được giao đúng hẹn và trong tình trạng tốt nhất.
+
+Tại Online Express Supermarket, chúng tôi luôn đặt sự hài lòng của khách hàng lên hàng đầu. Đội ngũ chăm sóc khách hàng của chúng tôi luôn sẵn lòng hỗ trợ và giải đáp mọi thắc mắc của khách hàng, từ việc tìm kiếm sản phẩm đến việc xử lý đơn hàng và trả lại hàng.
+
+Online Express Supermarket không chỉ là nơi để mua sắm, mà còn là điểm đến lý tưởng cho mọi người trong cuộc sống hàng ngày. Hãy đồng hành cùng chúng tôi và trải nghiệm sự tiện lợi và hài lòng mà chúng tôi mang lại!
+                   
+                </p>
+                <img src="./assets/image/e1234.jpg" alt="">
+            </div>
+        </div>
+    </article>
+</main>
